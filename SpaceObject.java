8d444dc45ec19dce0c84145a68066b5317365220
NathanPhilliber <Nathan@Nathans-MacBook-Pro.local>
2015-10-01 17:26:26 -0700

    Deleted test files

---
+++ SpaceObject.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class SpaceObject here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SpaceObject extends Object
+{
+    /**
+     * Act - do whatever the SpaceObject wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
