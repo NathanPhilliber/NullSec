97aaad17ee76acf31c4ba0c744b74487f7a23227
NathanPhilliber <Nathan@Nathans-MacBook-Pro.local>
2015-10-01 17:05:55 -0700

    Initial Upload

---
+++ Space.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Space here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Space extends World
+{
+
+    /**
+     * Constructor for objects of class Space.
+     * 
+     */
+    public Space()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(900, 450, 1, false); 
+    }
+}
