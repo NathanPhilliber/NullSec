5aacecae388e4082032ab7351f942114e139bd3c
Gorplex <Gorplexthefallen@gmail.com>
2015-10-02 15:54:51 -0700

    tutorial added

--- Space.java
+++ Space.java
@@ -23,12 +23,46 @@ public class Space extends World
         
         addObject(new AlienShip(0,0), 10,10);
         //addObject(new Planet(0,0),0,0);
+        
+        tutorial();
+    }
+    
+    public void tutorial()
+    {
+        
+        //EXAMPLE
+        //CHANGE AS NEEDED
+        showText("W to move forward", 600, 260);
+        showText("A and D to turn", 600, 280);
+        showText("C to shoot", 600, 300);
+        showText("space to boost", 600, 320);
+        showText("Q to swich weapon", 600, 340);
+        showText("numbers to change LV", 600, 360);
+        showText("shift to draw line", 600, 380);
+        showText("PRESS ANY KEY TO CONTINUE", 600, 440);
+    }
+    
+    public void act()
+    {
+        clearText();
+    }
+    
+    public void clearText()
+    {
+        if (Greenfoot.getKey() != null)
+        {
+            for (int i=0; i<=600; i+=20)
+            {
+                showText("",600,i);
+            }
+        }
     }
     
     //Return ship object
     public Ship getShip(){
         return ship;
     }
+    
     
     
 }
