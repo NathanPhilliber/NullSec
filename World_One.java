e0b074e5b6ddf072f4b13d5f087e4f1b6f76a3fd
NathanPhilliber <nphilliber@gmail.com>
2015-10-20 19:21:48 -0700

    Make all the platform levels inherit off of platform

---
+++ World_One.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class World_One here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class World_One extends Platformer
+{
+
+    /**
+     * Constructor for objects of class World_One.
+     * 
+     */
+    public World_One()
+    {
+    }
+}
---
+++ World_Two.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class World_Two here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class World_Two extends Platformer
+{
+
+    /**
+     * Constructor for objects of class World_Two.
+     * 
+     */
+    public World_Two()
+    {
+    }
+}
