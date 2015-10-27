41ad51d820d18007e6b03b23bf8d45cb191d64cf
NathanPhilliber <nphilliber@gmail.com>
2015-10-27 15:46:12 -0700

    Created another level example

--- Block.java
+++ Block.java
@@ -5,29 +5,73 @@ public class Block extends NonPlayer
     {
         super.act();
     }    
+
     public Block()
     {
         super();
     }
-    
+
     public Block(int blockType)
     {
         if(blockType == 0)
         {
-        setImage("brick.png");
+            setImage("brick.png");
         }
-        
+
         if(blockType == 1)
         {
-        setImage("BlueBrick.png");
+            setImage("BlueBrick.png");
+        }
+
+        if(blockType == 6){
+            setImage("MoonBrick.png");
+        }
+        if(blockType == 7){
+            setImage("MoonBrickTop.png");
+        }
+        if(blockType == 8){
+            setImage("GooTop.png");
+        }
+        if(blockType == 9){
+            setImage("GooBottom.png");
+        }
+        if(blockType == 10){
+            setImage("WaterTop.png");
+        }
+        if(blockType == 11){
+            setImage("WaterMiddle.png");
+        }
+        if(blockType == 12){
+            setImage("WaterBottom.png");
+        }
+        if(blockType == 13){
+            setImage("Dirt.png");
+        }
+        if(blockType == 14){
+            setImage("Grass.png");
+        }
+        if(blockType == 15){
+            setImage("SingleStone.png");
+        }if(blockType == 16){
+            setImage("StoneSquare.png");
+        }
+        if(blockType == 17){
+            setImage("GreyStone1.png");
+        }
+
+        if(blockType == 18){
+            setImage("GreyStone2.png");
+        }
+        if(blockType == 19){
+            setImage("SandTop.png");
+        }
+        if(blockType == 20){
+            setImage("SandMiddle.png");
         }
         
-        if(blockType == 2)
-        {
-        setImage("MoonBrick.png");
-        }
-        
+
     }  
+
     protected void addedToWorld(World world)
     {
         setRealX(getX());
