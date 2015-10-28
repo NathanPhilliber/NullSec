3b4d0ecd5121a10f2893d25c7c5d22ac08b1d12d
NathanPhilliber <nphilliber@gmail.com>
2015-10-28 11:09:46 -0700

    More features to level creator

--- LevelCreatorDisplayBlock.java
+++ LevelCreatorDisplayBlock.java
@@ -15,35 +15,45 @@ public class LevelCreatorDisplayBlock extends LevelCreatorObject
      */
     public static int block = 0;
 
+    private int delay = 0;
+
     public static String[] blockName = {"brick.png", "BlueBrick.png","Lava.png","Lava2.png", "StandingRight.png",
-        "Portal.png", "MoonBrick.png", "MoonBrickTop.png", "GooTop.png", "GooBottom.png", "WaterTop.png",  "WaterMiddle.png","WaterBottom.png",
-        "Dirt.png", "Grass.png", "SingleStone.png", "StoneSquare.png", "GreyStone1.png", "GreyStone2.png",
-        "SandTop.png", "SandMiddle.png",
-        "cancel.png"}; //MAKE CANCEL LAST
+            "Portal.png", "MoonBrick.png", "MoonBrickTop.png", "GooTop.png", "GooBottom.png", "WaterTop.png",  "WaterMiddle.png","WaterBottom.png",
+            "Dirt.png", "Grass.png", "SingleStone.png", "StoneSquare.png", "GreyStone1.png", "GreyStone2.png",
+            "SandTop.png", "SandMiddle.png",
+            "cancel.png"}; //MAKE CANCEL LAST
 
     public void act() 
     {
         MouseInfo m = Greenfoot.getMouseInfo();
-        if(Greenfoot.mouseClicked(this) && m.getButton() == 1){
-            block++;
-            if(block >= blockName.length){
-                block = 0;
+        delay++;
+
+        if(m != null){
+            
+            setLocation(m.getX(), getY());
+            
+            if((Greenfoot.mouseClicked(this) && m.getButton() == 1) || (Greenfoot.isKeyDown("w")&& delay > 15)){
+                delay = 0;
+                block++;
+                if(block >= blockName.length){
+                    block = 0;
+                }
+                setImage(blockName[block]);
+                List<LevelCreatorBlockHover> objects = getWorld().getObjects(LevelCreatorBlockHover.class);
+                objects.get(0).update(block);
             }
-            setImage(blockName[block]);
-            List<LevelCreatorBlockHover> objects = getWorld().getObjects(LevelCreatorBlockHover.class);
-            objects.get(0).update(block);
-        }
-        
-        if(Greenfoot.mouseClicked(this) && m.getButton() == 3){
-            block--;
-            if(block < 0){
-                block = blockName.length -1;
+
+            if((Greenfoot.mouseClicked(this) && m.getButton() == 3)|| (Greenfoot.isKeyDown("q") && delay > 15)){
+                delay = 0;
+                block--;
+                if(block < 0){
+                    block = blockName.length -1;
+                }
+                setImage(blockName[block]);
+                List<LevelCreatorBlockHover> objects = getWorld().getObjects(LevelCreatorBlockHover.class);
+                objects.get(0).update(block);
             }
-            setImage(blockName[block]);
-            List<LevelCreatorBlockHover> objects = getWorld().getObjects(LevelCreatorBlockHover.class);
-            objects.get(0).update(block);
         }
     }   
-    
-    
+
 }
