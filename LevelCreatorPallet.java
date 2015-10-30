acaa783b651af8decb435cd7f2ad6febc43fd966
NathanPhilliber <nphilliber@gmail.com>
2015-10-30 09:35:08 -0700

    Tweaks to level creator GUI

--- LevelCreatorPallet.java
+++ LevelCreatorPallet.java
@@ -15,10 +15,10 @@ public class LevelCreatorPallet extends LevelCreatorObject
     public int width = 25;
     public int length = 13; //height of pallet
     public int spaceBetween = 3;
-    
+
     public static int numPallets = 0;
     public static boolean open = false;
-    
+
     public LevelCreatorPallet(){
         numPallets++;
         open = true;
@@ -26,39 +26,46 @@ public class LevelCreatorPallet extends LevelCreatorObject
 
     public void act() 
     {
-        
+
     }  
 
     public void addedToWorld(World w){
         World world = getWorld();
-        
+
         if(numPallets > 0){
             numPallets--;
-            
+
             world.removeObjects(world.getObjects(LevelCreatorPalletBlock.class));
             world.removeObject(this);
         }
 
         int row = 0;
         int column = 0;
-        
+
         int mouseX = 0;
-        
+
         MouseInfo mouse = Greenfoot.getMouseInfo();
         if(mouse != null){
             mouseX = mouse.getX();
         }
-        
+
         for(int i = 0; i < LevelCreatorDisplayBlock.blockName.length; i++){
-            
-            world.addObject(new LevelCreatorPalletBlock(i), mouseX+25+row*(27+spaceBetween), 65+column*(27+spaceBetween));
-            
+
+            if(i == 4){
+                world.addObject(new LevelCreatorPalletBlock(i), mouseX+25+row*(27+spaceBetween), 65+17+column*(27+spaceBetween));
+            } 
+            else if(i == 5){
+                world.addObject(new LevelCreatorPalletBlock(i), mouseX-10+row*(27+spaceBetween), 65+5+column*(27+spaceBetween));
+            }
+            else{
+                world.addObject(new LevelCreatorPalletBlock(i), mouseX+25+row*(27+spaceBetween), 65+column*(27+spaceBetween));
+            }
             column++;
             if(column > length){
                 row++;
                 column = 0;
             }
         }
-        
+
     }
 }
