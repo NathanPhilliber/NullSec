d513fdeaafef5540b2f6cab985bdfadc65ea36ae
unknown <tracerainbolt@gmail.com>
2015-11-06 14:39:43 -0800

    Added bounce block visual and animation.

--- SlimeBlock.java
+++ SlimeBlock.java
@@ -8,14 +8,40 @@ import greenfoot.*;
  */
 public class SlimeBlock extends NoCollisionBlock
 {
+    private boolean playGif;
+    GifImage gifImage = new GifImage("BouncyGif.gif");
+    private final int gifLength = 9;
+    private int delay = gifLength;
+    
     public SlimeBlock(){
         this(88);
     }
     public SlimeBlock(int blockType){
-        setImage(LevelCreatorDisplayBlock.getBlock()[blockType]);
+        setImage("BounceBlock.png");
     }
     public void act() 
     {
         super.act();
-    }    
+        Actor s = getOneIntersectingObject(PlatformPlayer.class);
+        playBounceGif();
+        if(s != null)
+        {
+            delay = gifLength;
+            playGif = true;
+        }
+    }
+    
+    public void playBounceGif()
+    {
+        if(delay <= gifLength && playGif)
+        {
+            setImage(gifImage.getCurrentImage());
+            delay--;
+        }
+        if(delay <= 0)
+        {
+            playGif = false;
+            setImage("BounceBlock.png");
+        }
+    }
 }
