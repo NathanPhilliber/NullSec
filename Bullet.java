f96f6dbb84e4e205abe64f04e1c7b0e87d244103
NathanPhilliber <nphilliber@gmail.com>
2015-11-06 14:58:56 -0800

    Bullets destroy when hit by block

--- Bullet.java
+++ Bullet.java
@@ -3,7 +3,8 @@ public class Bullet extends MovingNonPlayer
 {
     private double speed=5;
     private boolean deleteMe = false;
-    
+    private boolean imOnscreenOnce = false;
+    private double lastX = -1;
     public void act() 
     {
         super.act();
@@ -11,30 +12,33 @@ public class Bullet extends MovingNonPlayer
         deleteOnEdge();
         deleteMe();
     }  
+
     public Bullet(int angle)
     {
         super();
         setRotation(angle);
     }
-    
-    
+
+        
     
     public void deleteOnEdge(){
         if(getX() > w.getWidth() || getX() < 0){
+            if(imOnscreenOnce){
+                deleteMe = true;
+            }
+        }
+        else{
+            imOnscreenOnce = true;
+        }
+        
+        if(lastX == getRealX()){
             deleteMe = true;
         }
+        lastX = getRealX();
     }
-    private void inBlock()
-    {
-        Actor b=getOneIntersectingObject(blockType);
-        if (b!=null)
-        {
-            addRealX(speed*Math.cos(getRotation()*2*Math.PI/360));
-            addRealX(speed*Math.sin(getRotation()*2*Math.PI/360));
-            //setLocation(speed*Math.cos(getRotation()*2*Math.PI/360)l
-        }
-    }
+
     
+
     public void deleteMe()
     {
         if ( deleteMe){
