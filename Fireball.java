ac3b238eb0d288b6eb24ac8d47d251af4d905bb3
Gorplex <Gorplexthefallen@gmail.com>
2015-11-17 10:30:08 -0800

    Update Fireball.java

--- Fireball.java
+++ Fireball.java
@@ -5,7 +5,7 @@ import greenfoot.*;
 public class Fireball extends Weapon implements ProjectileObject
 {
     private int beamAngle = 0;
-    
+    private int level=1;
     private double speed = 1.0;
 
 
@@ -22,16 +22,17 @@ public class Fireball extends Weapon implements ProjectileObject
     {
         super(angle, isPlayer, startX, startY, damage);
         despawnOnTouch = false;
+        level=1;
     }
     
     private void beam()
     {
         beamAngle++;
-        for (int i=0; i<=10; i++)
+        for (int i=0; i<=6*level; i++)
         {
             space.addObject(new Beam(beamAngle, true, 3.0, ship.getShipLocX(), ship.getShipLocY()), (int)Math.round(getX()+i*8*Math.cos(beamAngle*2*Math.PI/360)), (int)Math.round(getY()+i*8*Math.sin(beamAngle*2*Math.PI/360)));
         }
-        for (int i=0; i<=5; i++)
+        for (int i=0; i<=6*level; i++)
         {
             space.addObject(new Beam(beamAngle+180, true, 3.0, ship.getShipLocX(), ship.getShipLocY()), (int)Math.round(getX()+i*8*Math.cos((beamAngle+180)*2*Math.PI/360)), (int)Math.round(getY()+i*8*Math.sin((beamAngle+180)*2*Math.PI/360)));
         }
