c4e0769e5bdef479063284d1fc41b62898bebc16
NathanPhilliber <nphilliber@gmail.com>
2015-11-19 16:49:24 -0800

    Coin experimenting

--- Coin.java
+++ Coin.java
@@ -5,21 +5,29 @@ import greenfoot.*;
 public class Coin extends NoCollisionBlock
 {
     private GifImage coinAnimation = new GifImage("coinAnimation.gif");
+    //private int delay = 0;
     public Coin(){
         this(90);
     }
-    
+
     public Coin(int blockType){
         super();
         setImage(coinAnimation.getCurrentImage());
     }
-    
+
     public void act() 
     {
         super.act();
-        //setImage(coinAnimation.getCurrentImage());
+        /*
+        delay++;
+        if(delay>10){
+            delay = 0;
+            if(getX()>0 && getX()<w.getWidth())
+                setImage(coinAnimation.getCurrentImage());
+        }
+        */
     }  
-    
+
     public void pickup(){
         Player.goldPotential++;
         w.removeObject(this);
