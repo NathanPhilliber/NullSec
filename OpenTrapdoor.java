b08761b5490df8ecd007fdedcc035ced264e4408
NathanPhilliber <nphilliber@gmail.com>
2015-11-23 12:22:25 -0800

    Level 61 and made trap doors work with wire above and below it.

--- OpenTrapdoor.java
+++ OpenTrapdoor.java
@@ -15,15 +15,21 @@ public class OpenTrapdoor extends NoCollisionBlock
     }    
     public void updateImage()
     {
-        List<Actor> leftTrap = getObjectsAtOffset(-27, 0, Trapdoor.class);
-        List<Actor> rightTrap = getObjectsAtOffset(27, 0, Trapdoor.class);
+        
+        
         List<Actor> leftWire = getObjectsAtOffset(-27, 0, Wire.class);
         List<Actor> rightWire = getObjectsAtOffset(27, 0, Wire.class);
-        if(!(leftWire.isEmpty() || !rightWire.isEmpty()) || !leftTrap.isEmpty() || !rightTrap.isEmpty())
+        List<Actor> leftTrap = getObjectsAtOffset(-27, 0, Trapdoor.class);
+        List<Actor> rightTrap = getObjectsAtOffset(27, 0, Trapdoor.class);
+        List<Actor> upWire = getObjectsAtOffset(0, -27, Wire.class);
+        List<Actor> downWire = getObjectsAtOffset(0, 27, Wire.class);
+        List<Actor> upTrap = getObjectsAtOffset(0, -27, Trapdoor.class);
+        List<Actor> downTrap = getObjectsAtOffset(0, 27, Trapdoor.class);
+        if(!leftWire.isEmpty() || !rightWire.isEmpty() || !leftTrap.isEmpty() || !rightTrap.isEmpty() || !upWire.isEmpty() || !downWire.isEmpty() || !upTrap.isEmpty() || !downTrap.isEmpty())
         {
             Trapdoor trap = new Trapdoor();
             getWorld().addObject(trap, getX(), getY());
             getWorld().removeObject(this);
-        }          
+        }
     }
 }
--- Trapdoor.java
+++ Trapdoor.java
@@ -26,7 +26,11 @@ public class Trapdoor extends Block
         List<Actor> rightWire = getObjectsAtOffset(27, 0, WireOn.class);
         List<Actor> leftTrap = getObjectsAtOffset(-27, 0, OpenTrapdoor.class);
         List<Actor> rightTrap = getObjectsAtOffset(27, 0, OpenTrapdoor.class);
-        if(!leftWire.isEmpty() || !rightWire.isEmpty() || !leftTrap.isEmpty() || !rightTrap.isEmpty())
+        List<Actor> upWire = getObjectsAtOffset(0, -27, WireOn.class);
+        List<Actor> downWire = getObjectsAtOffset(0, 27, WireOn.class);
+        List<Actor> upTrap = getObjectsAtOffset(0, -27, OpenTrapdoor.class);
+        List<Actor> downTrap = getObjectsAtOffset(0, 27, OpenTrapdoor.class);
+        if(!leftWire.isEmpty() || !rightWire.isEmpty() || !leftTrap.isEmpty() || !rightTrap.isEmpty() || !upWire.isEmpty() || !downWire.isEmpty() || !upTrap.isEmpty() || !downTrap.isEmpty())
         {
             OpenTrapdoor trap = new OpenTrapdoor();
             getWorld().addObject(trap, getX(), getY());
